db5cd20b4b7384e3f9b1e000b2ed4e147276cce4
chaos <zw_luowei@126.com>
2019-04-01 23:57:49 +0800

    add page partition

--- notes/home.py
+++ notes/home.py
@@ -4,19 +4,21 @@ import os
 from flask import Blueprint
 from flask import render_template
 from flask import send_from_directory
+from flask import request
 
 import db
 import datetime
 from model.note import Note
 
+
+MAX_MSGS = 10
+
 bp = Blueprint('home', __name__, url_prefix='/home')
 
 
-@bp.route('/')
-def index():
-    # get all notes from the DB
+def get_msgs(offset, limit):
     session = db.get_session()
-    notes = session.query(Note).order_by(Note.id.desc())
+    notes = session.query(Note).order_by(Note.id.desc()).offset(offset).limit(limit)
     db.put_session()
     msgs = []
     if notes is not None:
@@ -25,9 +27,29 @@ def index():
             msg.setdefault('date', n.publish_date.strftime("%Y-%m-%d %H:%M:%S"))
             msg.setdefault('content', n.note)
             msgs.append(msg)
-    else:
+    return msgs
+
+
+@bp.route('/')
+def index():
+    # get all notes from the DB
+    msgs = get_msgs(0, MAX_MSGS)
+    if msgs and len(msgs) == 0:
         msg = dict()
         msg.setdefault('date', '')
         msg.setdefault('content',  u"没有任何信息")
         msgs.append(msg)
     return render_template('home/home.html', msgs=msgs)
+
+@bp.route('/next')
+def next():
+    offset = request.args.get('offset')
+    msgs = get_msgs(offset, MAX_MSGS)
+    if msgs and len(msgs) == 0:
+        msg = dict()
+        msg.setdefault('date', '')
+        msg.setdefault('content',  u"真的没了")
+        msgs.append(msg)
+    return render_template('home/home.html', msgs=msgs)
+
+

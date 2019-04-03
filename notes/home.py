290285b21eca53b9a50f76df6ef186c377dcada5
chaos <zw_luowei@126.com>
2019-04-03 23:21:57 +0800

    add delete message function

--- notes/home.py
+++ notes/home.py
@@ -16,7 +16,22 @@ MAX_MSGS = 10
 bp = Blueprint('home', __name__, url_prefix='/home')
 
 
-def get_msgs(offset, limit):
+def delete_msg(msg_id):
+    session = db.get_session()
+    msg = session.query(Note).filter(Note.id==msg_id).first()
+    session.delete(msg)
+    session.commit()
+    return
+
+
+def total_count():
+    session = db.get_session()
+    count = session.query(Note).count()
+    db.put_session()
+    return count
+
+
+def get_msgs(offset, limit=MAX_MSGS):
     session = db.get_session()
     notes = session.query(Note).order_by(Note.id.desc()).offset(offset).limit(limit)
     db.put_session()
@@ -24,6 +39,7 @@ def get_msgs(offset, limit):
     if notes is not None:
         for n in notes:
             msg = dict()
+            msg.setdefault('id', str(n.id))
             msg.setdefault('date', n.publish_date.strftime("%Y-%m-%d %H:%M:%S"))
             msg.setdefault('content', n.note)
             msgs.append(msg)
@@ -32,24 +48,38 @@ def get_msgs(offset, limit):
 
 @bp.route('/')
 def index():
+    count = total_count()
+    print "total count is %s" % count
     # get all notes from the DB
-    msgs = get_msgs(0, MAX_MSGS)
+    msgs = get_msgs(0)
     if msgs and len(msgs) == 0:
         msg = dict()
         msg.setdefault('date', '')
         msg.setdefault('content',  u"没有任何信息")
         msgs.append(msg)
-    return render_template('home/home.html', msgs=msgs)
+    return render_template('home/home.html', msgs=msgs, count=count)
+
 
 @bp.route('/next')
 def next():
     offset = request.args.get('offset')
-    msgs = get_msgs(offset, MAX_MSGS)
+    count = total_count()
+    msgs = get_msgs(offset)
     if msgs and len(msgs) == 0:
         msg = dict()
+        msg.setdefault('id', '')
         msg.setdefault('date', '')
-        msg.setdefault('content',  u"真的没了")
+        msg.setdefault('content',  u"哥，真的没了")
         msgs.append(msg)
-    return render_template('home/home.html', msgs=msgs)
+    return render_template('home/home.html', msgs=msgs, count=count)
+
+
+@bp.route('/delete')
+def delete():
+    msg_id = int(request.args.get('id'))
+    delete_msg(msg_id)
+    return next()
+
+
 
 
